6c90084f6e2260e40092d43c495eda12fb3bb122
Enrique Vidal <enrique@cloverinteractive.com>
2013-04-15 15:45:53 -0700

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'rails_env'
+maintainer       'cloverinteractive'
+maintainer_email 'enrique@cloverinteractive.com'
+license          'All rights reserved'
+description      'Installs/Configures rails environment'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
