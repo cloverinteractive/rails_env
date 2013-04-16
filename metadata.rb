23fa967475dafb3f73d9db715d7303a8bc8ede53
Enrique Vidal <enrique@cloverinteractive.com>
2013-04-16 01:26:12 -0700

    Freeze apt version

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Installs/Configures rails environment'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends 'apt'
+depends 'apt',                '1.7.0'
 depends 'ruby_build'
 depends 'rbenv'
 depends 'build-essential'
